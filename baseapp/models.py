6e14da75594b5c0dcf76a6b553ecc8adef9fe7e1
Adarsh Pandey <mail.apandey2001@gmail.com>
2022-02-04 11:16:05 +0530

    Updated models and serializers

--- baseapp/models.py
+++ baseapp/models.py
@@ -6,7 +6,11 @@ from django.utils.text import slugify
 
 class Course(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    one_line_description = models.CharField(max_length=300, blank=True, default="", help_text=('Describe the course in one but unforgettable line. Max 300 characters'))
     description = models.TextField(unique=True)
+    image_url = models.CharField(max_length=200, default="", help_text=('Enter Image URL within 200 characters.'))
+    high_price = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
     slug = models.SlugField(blank=True, unique=True, help_text=('Leave this parameter empty, it will get generated automatically.'))
 
     @property
@@ -15,7 +19,7 @@ class Course(models.Model):
     
     @property
     def short_description(self):
-        return truncatechars(self.description, 50)
+        return truncatechars(self.one_line_description, 50)
 
     def __str__(self):
         return truncatechars(self.name, 50)
@@ -26,6 +30,7 @@ class Course(models.Model):
 
 class Lecture(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    one_line_description = models.CharField(max_length=300, blank=True, default="", help_text=('Describe the lecture in one but unforgettable line. Max 300 characters'))
     description = models.TextField(unique=True)
     youtube_url = models.CharField(max_length=100, unique=True)
     course_index = models.IntegerField(unique=True)
